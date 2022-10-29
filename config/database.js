9b8c30e93dccc3d6519ae73e2623d5894794867b
Javier Cordero <76709688+javicl00@users.noreply.github.com>
2022-10-29 22:16:57 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,15 @@
+const { MongoClient } = require('mongodb');
+
 module.exports = {
-    database: 'mongodb+srv://javicl00:javicl00@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority',
+    const database = "mongodb+srv://javicl00:<password>@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority";
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+        // perform actions on the collection object
+        client.close();
+    }),
+
+//     database: 'mongodb+srv://javicl00:javicl00@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority',
     secret: 'yoursecret'
   }
   
