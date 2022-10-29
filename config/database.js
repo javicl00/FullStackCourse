76a6f058e61732fd18b56f2680725e480c085e39
Javier Cordero <76709688+javicl00@users.noreply.github.com>
2022-10-29 22:13:38 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database: 'mongodb+srv://javicl00:javicl00@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority,
+    database: 'mongodb+srv://javicl00:javicl00@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority',
     secret: 'yoursecret'
   }
   
