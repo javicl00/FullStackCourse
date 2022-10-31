a441f10e4875be29c919a26e74c557be83a2c68d
Javier Cordero <76709688+javicl00@users.noreply.github.com>
2022-10-31 11:53:52 +0200

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,17 @@
-const { MongoClient } = require('mongodb');
+// const aws = require('aws-sdk');
 
+// let s3 = new aws.S3({
+//   accessKeyId: process.env.S3_KEY,
+//   secretAccessKey: process.env.S3_SECRET
+// });
+
+// module.exports = {
+//   database: process.env.MONGO_URI,
+//   secret: process.env.SECRET,
+//   s3: s3
+// }  
 module.exports = {
-    const database = "mongodb+srv://javicl00:javicl00@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("test").collection("devices");
-        // perform actions on the collection object
-        client.close();
-    }),
+  database: "mongodb+srv://javicl00:javicl00@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority", //"http://localhost:27017/",
+  secret: "yoursecret"
+}
 
-//     database: 'mongodb+srv://javicl00:javicl00@fullstackcluster.jvg2mlo.mongodb.net/?retryWrites=true&w=majority',
-    secret: 'yoursecret'
-  }
-  
